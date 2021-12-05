895e7353f756d237336dfec316632fd15427e297
Daniel Nie <danielx.nie@gmail.com>
2021-12-05 14:01:50 -0500

    scrapy gets blocked even with user_agent rotations, looking at other data

--- daniel/redfin_scrapy/redfin/settings.py
+++ daniel/redfin_scrapy/redfin/settings.py
@@ -14,42 +14,18 @@ NEWSPIDER_MODULE = 'redfin.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-USER_AGENTS = [
-    ('Mozilla/5.0 (X11; Linux x86_64) '
-     'AppleWebKit/537.36 (KHTML, like Gecko) '
-     'Chrome/57.0.2987.110 '
-     'Safari/537.36'),  # chrome
-    ('Mozilla/5.0 (X11; Linux x86_64) '
-     'AppleWebKit/537.36 (KHTML, like Gecko) '
-     'Chrome/61.0.3163.79 '
-     'Safari/537.36'),  # chrome
-    ('Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:55.0) '
-     'Gecko/20100101 '
-     'Firefox/55.0'),  # firefox
-    ('Mozilla/5.0 (X11; Linux x86_64) '
-     'AppleWebKit/537.36 (KHTML, like Gecko) '
-     'Chrome/61.0.3163.91 '
-     'Safari/537.36'),  # chrome
-    ('Mozilla/5.0 (X11; Linux x86_64) '
-     'AppleWebKit/537.36 (KHTML, like Gecko) '
-     'Chrome/62.0.3202.89 '
-     'Safari/537.36'),  # chrome
-    ('Mozilla/5.0 (X11; Linux x86_64) '
-     'AppleWebKit/537.36 (KHTML, like Gecko) '
-     'Chrome/63.0.3239.108 '
-     'Safari/537.36'),  # chrome
-]
+
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 2.5
+DOWNLOAD_DELAY = 2.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -80,11 +56,11 @@ COOKIES_ENABLED = False
 DOWNLOADER_MIDDLEWARES = {
     'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None,
     'scrapy_useragents.downloadermiddlewares.useragents.UserAgentsMiddleware': 500,
-    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
-    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+    #'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+    #'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
 }
 
-ROTATING_PROXY_LIST_PATH = 'C:/Users/dn-83/Documents/bootcamp/capstone_proj/daniel/redfin_scrapy/proxy.txt'
+#ROTATING_PROXY_LIST_PATH = 'C:/Users/dn-83/Documents/bootcamp/capstone_proj/daniel/redfin_scrapy/proxy.txt'
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -98,18 +74,42 @@ ITEM_PIPELINES = {
     'redfin.pipelines.RedfinPipeline': 300,
 }
 
+USER_AGENTS = [
+    ('Mozilla/5.0 (Linux; Android 5.0.2; SM-T530NU Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.133 Safari/537.36'),
+    ('Mozilla/5.0 (Linux; Android 5.0.2; SAMSUNG SM-T530NU Build/LRX22G) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/3.2 Chrome/38.0.2125.102 Safari/537.36'),
+    ('Opera/9.80 (Linux armv6l; Opera TV Store/5599; (SonyBDP/BDV13)) Presto/2.12.362 Version/12.11'),
+    ('Mozilla/5.0 (Windows NT 6.1; rv:17.0) Gecko/20100101 Firefox/17.0'),
+    ('Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; .NET4.0C; .NET4.0E; InfoPath.3)'),
+    ('Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.1.4322; .NET CLR 2.0.50728)'),
+    ('Mozilla/5.0 (SMART-TV; X11; Linux armv7l) AppleWebKit/537.42 (KHTML, like Gecko) Chromium/25.0.1349.2 Chrome/25.0.1349.2 Safari/537.42'),
+    ('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:37.0) Gecko/20100101 Firefox/37.0'),
+    ('Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; WOW64; Trident/7.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; Media Center PC 6.0; InfoPath.3; .NET4.0C; .NET4.0E)'),
+    ('Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.130 Safari/537.36'),
+    ('Mozilla/5.0 (Mobile; Windows Phone 8.1; Android 4.0; ARM; Trident/7.0; Touch; rv:11.0; IEMobile/11.0; Microsoft; Lumia 535) like iPhone OS 7_0_3 Mac OS X AppleWebKit/537 (KHTML, like Gecko) Mobile Safari/537'),
+    ('Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; yie11; rv:11.0) like Gecko'),
+    ('Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; SLCC1; .NET CLR 2.0.50727; BRI/1; .NET CLR 3.5.30729; .NET CLR 3.0.30618)'),
+    ('Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; NP08; rv:11.0) like Gecko'),
+    ('Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.16 Safari/537.36 bdbrowser/1.9.0.2'),
+    ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'),
+    ('Opera/9.80 (Android; Opera Mini/6.1.27762/26.1849; U; es) Presto/2.8.119 Version/10.54'),
+    ('Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; BOIE9;ENUSMSNIP; rv:11.0) like Gecko'),
+    ('Mozilla/5.0 (Android; Mobile; rv:35.0) Gecko/35.0 Firefox/35.0'),
+    ('Links (2.1pre31; Linux 2.6.21-omap1 armv6l; x)'),
+    ('Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.2; WOW64; Trident/8.0; .NET4.0C; .NET4.0E; .NET CLR 2.0.50727; .NET CLR 3.0.30729; .NET CLR 3.5.30729)'),
+]
+
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 AUTOTHROTTLE_START_DELAY = 2
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 20
+# AUTOTHROTTLE_MAX_DELAY = 20
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
 # AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
--- daniel/redfin_scrapy/redfin/spiders/redfin_spider.py
+++ daniel/redfin_scrapy/redfin/spiders/redfin_spider.py
@@ -1,72 +1,91 @@
 from scrapy import Spider, Request
 from redfin.items import RedfinItem
+import pandas as pd
+import re
 
 base_redfin_url = 'https://www.redfin.com'
-zip_list = ['20001']
+ZIP_list = pd.read_csv('smh_zipcodes.csv', index_col='ZipCode').index #list of 6987 ZipCodes
 RF_item = RedfinItem() #initate items
 
 class RedfinSpider(Spider):
     name = 'redfin_spider'
     allowed_urls = ['https://redfin.com']
 
-    start_urls = [f'{base_redfin_url}/zipcode/{zip_code}' for zip_code in zip_list]
+    start_urls = [f'{base_redfin_url}/zipcode/{zip_code}' for zip_code in ZIP_list]
 
     def parse(self, response):
-        house_urls = response.xpath('//a[@class="slider-item "]/@href').extract()
-        result_urls = [f'{base_redfin_url}{house}' for house in house_urls]
+        # obtain number of pages in ZIP search
+        page_text = response.xpath("//div[@class='HomeViews']//span[@class='pageText']/text()").extract_first()
+        total_pages = int(re.findall('\d+$',page_text)[0])
+        
+        page_urls = [f'{response.url}/page-{n}' for n in range(1,total_pages+1)]
+
+        for url in page_urls:
+            yield Request(url=url, callback=self.parse_results_page)
+    
+    def parse_results_page(self, response):
+        # obtain house links for each page of search
+        house_links = response.xpath('//a[@class="slider-item "]/@href').extract()
+        result_urls = [f'{base_redfin_url}{house}' for house in house_links]
 
         for url in result_urls:
-            yield Request(url=url, callback=self.parse_results_page)
+            yield Request(url=url, callback=self.parse_house_page)
 
-    def parse_results_page(self, response):
+    def parse_house_page(self, response):
         # extracts PROP ADDRESS from title
-        RF_item['Prop_Addr'] = response.xpath('//h1/div[@class="street-address"]/@title').extract()[0]
+        RF_item['Prop_Addr'] = response.xpath('//h1/div[@class="street-address"]/@title').extract_first()
         # extracts LISTPRICE
-        RF_item['ListPrice'] = response.xpath("//div[@class='statsValue']/text()").extract()[0]
+        RF_item['ListPrice'] = response.xpath("//div[@class='statsValue']/text()").extract_first()
         # extracts PROP DESCRIPTION
-        RF_item['Text_base'] = response.xpath("//p[@class='text-base']/span/text()").extract()[0]
+        RF_item['Text_base'] = response.xpath("//p[@class='text-base']/span/text()").extract_first()
+        # extracts ZipCode
+        RF_item['ZipCode'] = response.xpath("//h1/div[@class='dp-subtext']/text()").re('\d+')[0]
+
+        # Extracts MLS number
+        MLS_loc = response.xpath("//div[@class='keyDetailsList']\
+                                //span[@class='header font-color-gray-light inline-block']/text()").extract().index('MLS#')
+        RF_item['MLS_num'] = response.xpath("//div[@class='keyDetailsList']//span[@class='content text-right']/text()").extract()[MLS_loc-1]
 
         # extracts HOME FACTS table information
-        home_facts = response.xpath("//div[@class='keyDetailsList']//span[@class='content text-right']/text()").extract()
-        RF_item['Time_onRF'] = home_facts[0]
-        RF_item['Prop_type'] = home_facts[1]
-        RF_item['HOA_dues'] = home_facts[2]
-        RF_item['Year_built'] = home_facts[3]
-        RF_item['Prop_style'] = home_facts[4]
-        RF_item['Community'] = home_facts[5]
-        RF_item['MLS_num'] = home_facts[6]
-        # extracts PRICE INSIGHTS table information
-        RF_item['ListPrice2'] = home_facts[7]
-        RF_item['EstMo_pmt'] = home_facts[8]
-        if len(home_facts) == 11:
-            RF_item['RF_PriceEst'] = 'NaN'
-            RF_item['Price_perSF'] = home_facts[9]
-            RF_item['Buyers_comm'] = home_facts[10]
-        else:
-            RF_item['RF_PriceEst'] = home_facts[9]
-            RF_item['Price_perSF'] = home_facts[10]
-            RF_item['Buyers_comm'] = home_facts[11]
+        # home_facts = response.xpath("//div[@class='keyDetailsList']//span[@class='content text-right']/text()").extract()
+        # RF_item['Time_onRF'] = home_facts[0]
+        # RF_item['Prop_type'] = home_facts[1]
+        # RF_item['HOA_dues'] = home_facts[2]
+        # RF_item['Year_built'] = home_facts[3]
+        # RF_item['Prop_style'] = home_facts[4]
+        # RF_item['Community'] = home_facts[5]
+        # RF_item['MLS_num'] = home_facts[6]
+        # # extracts PRICE INSIGHTS table information
+        # RF_item['ListPrice2'] = home_facts[7]
+        # RF_item['EstMo_pmt'] = home_facts[8]
+        # if len(home_facts) == 11:
+        #     RF_item['RF_PriceEst'] = 'NaN'
+        #     RF_item['Price_perSF'] = home_facts[9]
+        #     RF_item['Buyers_comm'] = home_facts[10]
+        # else:
+        #     RF_item['RF_PriceEst'] = home_facts[9]
+        #     RF_item['Price_perSF'] = home_facts[10]
+        #     RF_item['Buyers_comm'] = home_facts[11]
 
         # extracts PUBLIC FACTS table information
-        public_facts = response.xpath("//div[@class='facts-table']//div[@class='table-value']/text()").extract()
-        RF_item['Tot_beds'] = public_facts[0]
-        RF_item['Tot_baths'] = public_facts[1]
-        RF_item['Fin_sqft'] = public_facts[2]
-        RF_item['Unf_sqft'] = public_facts[3]
-        RF_item['Tot_sqft'] = public_facts[4]
-        RF_item['Stories'] = public_facts[5]
-        RF_item['Lot_size'] = public_facts[6]
-        RF_item['Prop_style2'] = public_facts[7]
-        RF_item['YearBuilt2'] = public_facts[8]
-        RF_item['YearReno'] = public_facts[9]
-        RF_item['County'] = public_facts[10]
-        RF_item['APN_num'] = public_facts[11]
+        # public_facts = response.xpath("//div[@class='facts-table']//div[@class='table-value']/text()").extract()
+        # RF_item['Tot_beds'] = public_facts[0]
+        # RF_item['Tot_baths'] = public_facts[1]
+        # RF_item['Fin_sqft'] = public_facts[2]
+        # RF_item['Unf_sqft'] = public_facts[3]
+        # RF_item['Tot_sqft'] = public_facts[4]
+        # RF_item['Stories'] = public_facts[5]
+        # RF_item['Lot_size'] = public_facts[6]
+        # RF_item['Prop_style2'] = public_facts[7]
+        # RF_item['YearBuilt2'] = public_facts[8]
+        # RF_item['YearReno'] = public_facts[9]
+        # RF_item['County'] = public_facts[10]
+        # RF_item['APN_num'] = public_facts[11]
 
         # extracts TRANSPORTATION Info
-        transportation = response.xpath("//div[@class='walk-score']//div[@class='percentage']/span/text()").extract()
+        transportation = response.xpath("//div[@class='walk-score']//div[@class='percentage']/span/text()").re('\d+')
         RF_item['Walk_score'] = transportation[0]
         RF_item['Transit_score'] = transportation[2]
         RF_item['Bike_score'] = transportation[4]
-
 
         yield RF_item
