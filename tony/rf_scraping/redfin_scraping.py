f095ebd52e2369a21b9c402515506fd4ff40cb6f
Tony Pennoyer <tonypennoyer@gmail.com>
2021-12-06 10:08:31 -0500

    selenium scraper for csvs

--- tony/rf_scraping/redfin_scraping.py
+++ tony/rf_scraping/redfin_scraping.py
@@ -14,13 +14,15 @@ from itertools import chain
 from csv import writer
 from bs4 import BeautifulSoup
 
-def make_soup(url) :
+def make_soup(zips) :
 	options = Options()
 	options.headless = True
 
 	DRIVER_PATH = '/Users/tonypennoyer/desktop/capstone/chromedriver'
 	driver = webdriver.Chrome(options=options, executable_path=DRIVER_PATH)
+	base = 'https://www.redfin.com/zipcode/'
 
+	url = base + zips
 
 	try :
 
@@ -30,7 +32,7 @@ def make_soup(url) :
 
 		gotit.click()
 
-		time.sleep(1)
+		sleep(1)
 
 		driver.close()
 
@@ -41,6 +43,26 @@ def make_soup(url) :
 
 
 
-zip_list = [12581,22400,91945]
+df = pd.read_csv('zip_df.csv')
+df['ZipCode'] = df['ZipCode'].apply(str)
+zip_list = df.ZipCode
+count = 0
 
-make_soup('https://www.redfin.com/zipcode/12550')
+for zip_code in zip_list:
+	if count < 70001 :
+		print(zip_code)
+		make_soup(zip_code)
+		count = count + 1
+	else : print('done')
+
+
+
+
+
+
+
+
+
+
+
+
