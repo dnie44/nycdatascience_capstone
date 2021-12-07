024e924a5cd403404938fd11132ddd8b18b963e3
Jack Copeland <jackcopeland11@gmail.com>
2021-12-07 09:32:55 -0500

    Scrapy working for great schools. Data saved as JSON file

--- jack/Scrapy/schools/schools/spiders/school_scraper.py
+++ jack/Scrapy/schools/schools/spiders/school_scraper.py
@@ -1,24 +1,38 @@
 import scrapy 
+from scrapy import Spider, Request
 import re
+import pandas as pd
 import json
 
+zips = pd.read_csv('smh_zips.txt')
+zip_codes = zips['ZipCode']
+zip_codes =zip_codes.tolist()
+    
 class SchoolSpider(scrapy.Spider):
     name = "school_scraper"
-    num_pages = 1
-
-    ## Need to get the links for zips and pages -- right now it only looks at the first page of one zip
-    links = []
-    for i in range(1,3):
-        link = 'https://www.greatschools.org/search/search.zipcode?page=' + str(i) + '&sort=rating&zip=20852'
-        links.append(link)
-    start_urls = links
+    # start_urls = [f'https://www.greatschools.org/search/search.zipcode?page=1&sort=rating&zip={zip}' for zip in zip_codes]
+    start_urls = [f'https://www.greatschools.org/search/search.zipcode?page=1&sort=rating&zip=20852']
 
     def parse(self, response):
-
         res = response.xpath("//script").extract_first()
         x = json.loads(re.search(r'gon.search=(.*?);', res).group(1))
-        schools = x['schools']
-        print(len(schools))
+        num_pages = x['totalPages'] 
+        num_pages += 1
+
+        page_urls = ['https://www.greatschools.org/search/search.zipcode?page=' + str(i) + '&sort=rating&zip=' + str(z) for i in range(1, num_pages) for z in zip_codes]
+
+        print('pageurls =', page_urls)
+
+        for url in page_urls:
+            yield Request(url = url, callback = self.parse_results)
+
+    def parse_results(self, response):
+
+        res_2 = response.xpath("//script").extract_first()
+        y = json.loads(re.search(r'gon.search=(.*?);', res_2).group(1))
+        schools = y['schools']
+
+        dict_list = []
 
         dict = {}
         for i in range(0, len(schools)):
@@ -38,5 +52,4 @@ class SchoolSpider(scrapy.Spider):
             dict['schoolType'] = temp['schoolType']
             dict['pctLowIncome']= temp['percentLowIncome']
             dict['overallRating']  = temp['rating']
-
             yield dict
